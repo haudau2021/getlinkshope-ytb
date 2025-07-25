7907ca1d89e31f31d82fc47c2c2619b9e6d57adf
haudau2021 <duahau121121@gmail.com>
2025-07-26 01:29:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def index():
     return render_template("index.html", now=datetime.now())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="0.0.0.0")
